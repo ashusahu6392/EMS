0e105efb405adbd65204a58ac47ea38eb2f55e3f
Ashu Sahu <Ashusahu6392@gmail.com>
2025-12-06 23:40:26 +0530

    Added a Employee.java file full

--- WebApp/src/main/java/com/app/entity/Employee.java
+++ WebApp/src/main/java/com/app/entity/Employee.java
@@ -1,5 +1,127 @@
 package com.app.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private int emp_id;
+	private String fname;
+	private String lname;
+	private String email;
+	private String phone;
+	private String department;
+	private String designation;
+	private boolean isActive;
+	private double salary;
+	
+	 public Employee() {
+		 this.isActive = true;
+	 }
+
+	public Employee(String fname, String lname, String email, String phone, String department, String designation,
+			double salary) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.phone = phone;
+		this.department = department;
+		this.designation = designation;
+		this.salary = salary;
+		this.isActive = true;
+	}
+
+	public int getEmp_id() {
+		return emp_id;
+	}
+
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [emp_id=" + emp_id + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", phone="
+				+ phone + ", department=" + department + ", designation=" + designation + ", isActive=" + isActive
+				+ ", salary=" + salary + "]";
+	}
+	
+	
+	 
+	 
+	
 
 }
