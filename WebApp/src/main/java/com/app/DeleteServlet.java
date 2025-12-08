0c8c578ae88891df2be64668c2b22ab98b260a27
Ashu Sahu <Ashusahu6392@gmail.com>
2025-12-08 13:10:11 +0530

    Added a DeleteServlet file to Delete Employee Details

---
+++ WebApp/src/main/java/com/app/DeleteServlet.java
@@ -0,0 +1,24 @@
+package com.app;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class DeleteServlet extends HttpServlet {
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
